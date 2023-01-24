178aaf72f1d183a5f887187eb83d976d4f38ac1a
Liudmyla_Tuteiko <ICLiudmyla.Tuteiko@americanwell.com>
2023-01-24 15:18:05 +0200

    Improved test with random locators

---
+++ tests/productPurchaseTest.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect, Locator } from '@playwright/test';
+
+test('Smoke test', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#toggle_button').click();
+
+  const menuItems = page.locator('.ammenu-nav-sections > .ammenu-items > li > a');
+  await clickRandomElFromList(menuItems);
+
+  const productCategoryLinks = page.locator('.supercat-links a');
+  await clickRandomElFromList(productCategoryLinks);
+
+  const productLinks = page.locator('.product-item-link');
+  await clickRandomElFromList(productLinks);
+
+  const productName = page.locator('.page-title');
+
+  await page.locator('.tocart').click();
+  await page.locator('.showcart').click();
+
+  await expect(page.locator('.product-item-name a')).toHaveText(await productName.textContent());
+});
+
+
+
+async function clickRandomElFromList(list: Locator) {
+  const randomElOrder = Math.floor(Math.random() * ((await list.count()-1) - 0) + 0);
+  await list.nth(randomElOrder).click();
+}
