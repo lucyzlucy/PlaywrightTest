9d45be373f1b8fd0b4dc244ee957677d07d6a86f
Liudmyla_Tuteiko <ICLiudmyla.Tuteiko@americanwell.com>
2023-01-24 15:18:22 +0200

    Refactoring

--- tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Туристичне спорядження, одяг і взуття від найкращих брендів | Gorgany - експерти з туризму/);
-});
