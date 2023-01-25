f7721e07ed7abfd97f2ce4a711101a5010ff734b
Liudmyla_Tuteiko <ICLiudmyla.Tuteiko@americanwell.com>
2023-01-25 17:58:34 +0200

    Added screenshots and traces capturing

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,8 @@ const config: PlaywrightTestConfig = {
     baseURL: 'https://www.gorgany.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
@@ -95,7 +96,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
