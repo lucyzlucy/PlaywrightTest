ac3206f4415b575acd28d99e903688349a9bb71b
Liudmyla_Tuteiko <ICLiudmyla.Tuteiko@americanwell.com>
2023-02-03 18:51:22 +0200

    TEST2

--- tests/productPurchaseTestV2.spec.ts
+++ tests/productPurchaseTestV2.spec.ts
@@ -3,8 +3,7 @@ import { ProductCataloguePage } from '../pages/ProductCataloguePage';
 import { allure, LabelName } from "allure-playwright";
 
 test('Smoke test refactored', async ({ page }, testoptions) => {
-    process.env.browser = testoptions.project.name;
-    allure.label({ name: LabelName.TAG, value: "$testoptions.project.name" })
+    allure.label({ name: LabelName.TAG, value: `$testoptions.project.name` })
     const catalogue = new ProductCataloguePage(page);
     await catalogue.goto();
     await catalogue.openMenu();
@@ -17,6 +16,4 @@ test('Smoke test refactored', async ({ page }, testoptions) => {
     await productPage.openCartPreview();
     await expect(productPage.productName).toBeVisible();
     await expect(productPage.bucketProductName).toHaveText(await productPage.productName.textContent());
-
-    console.log(process.env.browser);
 });
