ba17fef86fd041833ffcdfa433c4ed0e20935f73
Liudmyla_Tuteiko <ICLiudmyla.Tuteiko@americanwell.com>
2023-02-03 14:01:02 +0200

    Added label

--- tests/productPurchaseTestV2.spec.ts
+++ tests/productPurchaseTestV2.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { MainPage } from '../pages/MainPage';
+import { allure, LabelName } from "allure-playwright";
 import { ProductCataloguePage } from '../pages/ProductCataloguePage';
 
 
-test('Smoke test refactored', async ({ page }) => {
+test('Smoke test refactored', async ({ page }, testInfo) => {
+    const env = testInfo.project.name;
+    allure.id("Smoke Test");
+    allure.label({
+        "name": "environment",
+        "value": `$env`
+      })
     const catalogue = new ProductCataloguePage(page);
     await catalogue.goto();
     await catalogue.openMenu();
