96330d7c8e507bbd5c1e1cbe7c9c489d49cd3997
Liudmyla_Tuteiko <ICLiudmyla.Tuteiko@americanwell.com>
2023-06-20 16:31:37 +0300

    Added more logging

--- utils/Helpers.ts
+++ utils/Helpers.ts
@@ -2,7 +2,8 @@ import { Locator } from '@playwright/test';
 
 export async function clickRandomElFromList(list: Locator) {
     const randomElOrder = Math.floor(Math.random() * ((await list.count()-1) - 0) + 0);
+    console.log(`Element order chosen: ${randomElOrder}`);
     const randomEl = list.nth(randomElOrder);
-    console.log(await randomEl.allInnerTexts())
+    console.log(await randomEl.allInnerTexts());
     await randomEl.click();
   }
